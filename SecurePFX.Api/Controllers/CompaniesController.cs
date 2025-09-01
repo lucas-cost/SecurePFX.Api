bd7a1d01926d719fde2f9a9814295c34cb8c73d4
lucas-cost <lucas_costa@alu.ufc.br>
2025-09-01 15:58:31 -0300

    feat(api): adicionar CompaniesController

---
+++ SecurePFX.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SecurePFX.Api.Resources;
+using SecurePFX.Application.DTOs.Requests;
+using SecurePFX.Application.DTOs.Responses;
+using SecurePFX.Application.Interfaces;
+
+namespace SecurePFX.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyService _companyService;
+        private readonly ILogger<CompaniesController> _logger;
+        private readonly IMapper _mapper;
+
+        public CompaniesController(ICompanyService companyService, ILogger<CompaniesController> logger, IMapper mapper)
+        {
+            _companyService = companyService;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCompany([FromBody] CreateCompanyRequestDTO request)
+        {
+            try
+            {
+                CompanyResponseDTO response = await _companyService.CreateCompanyAsync(request);
+
+                return Ok(new
+                {
+                    Success = true,
+                    Data = response
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return BadRequest(new { Success = false, ex.Message });
+            }
+        }
+    }
+}
--- SecurePFX.Api/Program.cs
+++ SecurePFX.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using SecurePFX.Api.Extensions;
 using SecurePFX.Api.Mapping;
@@ -6,6 +7,7 @@ using SecurePFX.Application.Mapping;
 using SecurePFX.Application.Services;
 using SecurePFX.Application.Settings;
 using SecurePFX.Domain.Interfaces.Repositories;
+using SecurePFX.Domain.Interfaces.UoW;
 using SecurePFX.Infrastructure.Authentication;
 using SecurePFX.Infrastructure.Data.Contexts;
 using SecurePFX.Infrastructure.Data.Repositories;
@@ -23,6 +25,8 @@ builder.Services.AddControllers()
 // Configurações
 builder.Services.Configure<CertificateSettings>(builder.Configuration.GetSection("CertificateSettings"));
 builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Autenticação JWT
 builder.Services.AddJwtAuthentication(builder.Configuration);
@@ -30,6 +34,7 @@ builder.Services.AddJwtAuthentication(builder.Configuration);
 // AutoMapper
 builder.Services.AddAutoMapper(typeof(CertificateProfile));
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddAutoMapper(typeof(CompanyProfile));
 
 // MongoDB
 builder.Services.AddSingleton<MongoDbContext>(provider =>
@@ -38,12 +43,16 @@ builder.Services.AddSingleton<MongoDbContext>(provider =>
     return new MongoDbContext(Options.Create(settings));
 });
 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 // Repositórios
 builder.Services.AddScoped<ICertificateRepository, CertificateRepository>();
-builder.Services.AddScoped<IEncryptionService, EncryptionService>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();    
 
 // Serviços de Aplicação
 builder.Services.AddScoped<ICertificateService, CertificateService>();
+builder.Services.AddScoped<IEncryptionService, EncryptionService>();
+builder.Services.AddScoped<ICompanyService, CompanyService>();
 
 // Swagger (apenas para desenvolvimento)
 if (builder.Environment.IsDevelopment())
