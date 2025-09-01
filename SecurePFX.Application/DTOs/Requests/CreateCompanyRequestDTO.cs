acf5897827a1f70e92716bb0865c2494a5b47030
lucas-cost <lucas_costa@alu.ufc.br>
2025-09-01 16:00:36 -0300

    feat(dtos): adicionar dots de request e response

---
+++ SecurePFX.Application/DTOs/Requests/CreateCompanyRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecurePFX.Application.DTOs.Requests
+{
+    public class CreateCompanyRequestDTO
+    {
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$", ErrorMessage = "CNPJ inválido. Formato esperado: 00.000.000/0000-00")]
+        public string CNPJ { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+    }
+}
---
+++ SecurePFX.Application/DTOs/Responses/CompanyResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SecurePFX.Application.DTOs.Responses
+{
+    public class CompanyResponseDTO
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string CNPJ { get; set; }
+        public string Email { get; set; }
+        public bool IsPayingCustomer { get; set; }
+    }
+}
