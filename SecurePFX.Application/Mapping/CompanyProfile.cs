222794edb62c86a71abe785203bb1f5131ce6501
lucas-cost <lucas_costa@alu.ufc.br>
2025-09-01 16:01:13 -0300

    feat(mapping): adicionar mapeamento do response para company

---
+++ SecurePFX.Application/Mapping/CompanyProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SecurePFX.Application.DTOs.Requests;
+using SecurePFX.Application.DTOs.Responses;
+using SecurePFX.Domain.Entities;
+
+namespace SecurePFX.Application.Mapping
+{
+    public class CompanyProfile : Profile
+    {
+        CompanyProfile()
+        {
+            CreateMap<CreateCompanyRequestDTO, Company>();
+            CreateMap<Company, CompanyResponseDTO>();
+        }
+    }
+}
---
+++ SecurePFX.Domain/Entities/Company.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecurePFX.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string CNPJ { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsPayingCustomer { get; set; }
+    }
+}
