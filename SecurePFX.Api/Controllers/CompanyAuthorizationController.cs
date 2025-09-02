40f3063f5b709754e1a6a84a705bb76728f80ed0
lucas-cost <lucas_costa@alu.ufc.br>
2025-09-02 20:44:07 -0300

    feat(api): adicionar estrutura inicial do controller de autorização

---
+++ SecurePFX.Api/Controllers/CompanyAuthorizationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecurePFX.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyAuthorizationController : ControllerBase
+    {
+        private readonly ILogger<CompanyAuthorizationController> _logger;
+
+        public CompanyAuthorizationController(ILogger<CompanyAuthorizationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AuthorizeCompany()
+        {
+            return Ok("SecurePFX Api is running...");
+        }
+    }
+}
