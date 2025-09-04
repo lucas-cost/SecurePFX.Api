0e30bb2d2ad985f3ec39d5a6fdd9af976d61059f
lucas-cost <lucas_costa@alu.ufc.br>
2025-09-04 08:34:01 -0300

    refactor(response): remover atributo id

--- SecurePFX.Application/DTOs/Responses/CompanyResponseDTO.cs
+++ SecurePFX.Application/DTOs/Responses/CompanyResponseDTO.cs
@@ -2,7 +2,6 @@
 {
     public class CompanyResponseDTO
     {
-        public int Id { get; set; }
         public string CompanyName { get; set; }
         public string CNPJ { get; set; }
         public string Email { get; set; }
